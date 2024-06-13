933dd83e235512e5ccb56c52543e9cd6e3080f9b
EduardoZF <eduardozf1@gmail.com>
2024-06-13 18:14:44 -0300

    + Added: Api prefix on all routes

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors()); // TODO add cors URL when pushing to production
 
 // Add application routes
-app.use(routes);
+app.use("/api", routes);
 
 // Handle all application erros
 app.use(errorHandler);
